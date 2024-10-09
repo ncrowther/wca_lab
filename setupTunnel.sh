7fdd16f173de15691d5276b2331a6ddc58ca37bd
Bamoe <bamoe@ibm.com>
2024-10-09 17:19:21 +0100

    Update

--- setupTunnel.sh
+++ setupTunnel.sh
@@ -11,3 +11,8 @@ scp ${pubkey} root@158.175.108.28:.
 
 ssh root@158.175.108.28
 
+# On the remote system enter: 
+# cat [INITIALS]_rsa.pub >> ~/.ssh/authorized_keys
+# Exit the remote session by hitting Ctl-d
+# Back in the original terminal, should be able to SSH to your remote account from the computer without supplying a password. Check this by typing:
+# ssh root@158.175.108.28
